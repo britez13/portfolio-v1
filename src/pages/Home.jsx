be62185a627720a7bddec9e5726fc927a2f0422b
Florencio Britez <florenciobritez49@gmail.com>
2022-12-12 11:42:16 -0300

    Change animation behavior

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,10 @@ import { gsap } from "gsap";
 import "../styles/Home.scss";
 
 const Home = () => {
-
   useEffect(() => {
-    gsap.to(".title > span", { y:0, duration: 1.5, delay: 2.1, stagger: 1 });
-    gsap.to(".title .name", { y: 0, duration: 1.5, delay: 2.2});
-    gsap.to(".home-image", { opacity: 1, duration: 8, delay: 4.5 })
+    gsap.to(".title > span", { y: 0, duration: 1.5, delay: 2.1, stagger: 1 });
+    gsap.to(".title .name", { y: 0, duration: 1.5, delay: 2.2 });
+    gsap.to(".home-image", { opacity: 1, duration: 8, delay: 5.1 });
   });
 
   return (
@@ -21,7 +20,9 @@ const Home = () => {
             I'm <span className='name'>Florencio</span>,
           </span>
         </h1>
-        <h1 className='title'><span>Frontend Dev</span></h1>
+        <h1 className='title'>
+          <span>Frontend Dev</span>
+        </h1>
       </section>
       <section>
         <img
