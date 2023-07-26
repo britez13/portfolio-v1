6462f6024db2294de996de5ec9c073233052167e
Florencio Britez <florenciobritez49@gmail.com>
2023-07-26 14:37:07 -0400

    Add more spacing to project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -99,12 +99,14 @@ const ProjectCard = ({ project, elementsArray }) => {
         <p className='stack'>
           Stack: <span>{project.stack}</span>{" "}
         </p>
+        <div className="links">
         <a className="link" href={project.liveURL} target='_blank'>
           See Demo
         </a>
         <a className="link" href={project.codeURL} target='_blank'>
           See Code
         </a>
+        </div>
       </div>
     </div>
   );
